e7eaff4e4431408a48a58e30faa28fc1dab5de90
xaridar <xaridara@gmail.com>
2022-02-12 03:00:48 -0500

    refactor to node

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    if (
+        !req.secure &&
+        req.get('x-forwarded-proto') !== 'https' &&
+        process.env.NODE_ENV === 'production'
+    ) {
+        return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+});
+
+app.use(express.static('./public', { extensions: 'html' }));
+
+const port = process.env.PORT || 1000;
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}.`);
+});
