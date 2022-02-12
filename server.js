2f9a5c76f98fdc0e2358e64d53b526f644ade5f4
xaridar <xaridara@gmail.com>
2022-02-12 03:08:07 -0500

    final test

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    if (
-        !req.secure &&
-        req.get('x-forwarded-proto') !== 'https' &&
-        process.env.NODE_ENV === 'production'
-    ) {
-        console.log(req.hostname);
-        next();
-        // return res.redirect(`https://${req.hostname}${req.url}`);
-    }
-});
-
-app.use(express.static('./public', { extensions: 'html' }));
+app.use(express.static('./public'));
 
 const port = process.env.PORT || 1000;
 app.listen(port, () => {
