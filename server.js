6ecd00f80a55f948ccd8ad78495e1119d754ba4e
xaridar <xaridara@gmail.com>
2022-02-12 11:54:14 -0500

    test ssl requirement

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const compression = require('compression');
 
 const app = express();
+
+app.use(compression());
+
+app.use((req, res, next) => {
+    if (
+        !req.secure &&
+        req.headers['x-forwarded-proto'] != 'https' &&
+        process.env.NODE_ENV !== 'development'
+    ) {
+        return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
 
 app.use(express.static('./public'));
 
